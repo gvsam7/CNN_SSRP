c5c3665b47af3cb68130fbaee57e15f710ec5dc7
Georgios Voulgaris <gvsam7@yahoo.com>
2020-02-14 17:03:50 +0000

    Multiclass cnn

--- CNN.py
+++ CNN.py
@@ -14,9 +14,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import time
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 
-REBUILD_DATA = True  # set to true to once, then back to false unless I want to change something in my training data.
+REBUILD_DATA = True  # set to true once, then back to false unless I want to change something in my training data.
 
 
 class Barren_landVSWater:
@@ -128,30 +131,106 @@ print(f"len(train x): {len(train_X)}")
 print(f"len test_x: {len(test_X)}")
 
 
+def fwd_pass(X, y, train=False):  #  train=False preventsmodifing weights when we do our validation data
+    outputs = net(X)
+    acc = (outputs.argmax(dim=1) == y.argmax(dim=1)).float().mean()
+    # matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, y)]
+    # acc = matches.count(True) / len(matches)
+    loss = loss_function(outputs, y)
+
+    if train:
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    return acc, loss
+
+MODEL_NAME = f"model-{int(time.time())}" # gives a dynamic model name, to
+
 def train(net):
     BATCH_SIZE = 100
-    EPOCHS = 3
-    for epoch in range(EPOCHS):
-        for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
-            batch_X = train_X[i: i + BATCH_SIZE].view(-1, 1, 50, 50)
-            batch_y = train_y[i: i + BATCH_SIZE]
+    EPOCHS = 100
 
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+    with open("model.log", "a") as f:
+        for epoch in range(EPOCHS):
+            for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+                batch_X = train_X[i: i + BATCH_SIZE].view(-1, 1, 50, 50)
+                batch_y = train_y[i: i + BATCH_SIZE]
 
-            net.zero_grad()
-            outputs = net(batch_X)
+                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
 
-            matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, batch_y)]
-            in_sample_acc = matches.count(True) / len(matches)
+                acc, loss = fwd_pass(batch_X, batch_y, train=True)
 
-            loss = loss_function(outputs, batch_y)
-            loss.backward()
-            optimizer.step()
-        print(loss)
-        print("In-sample acc:", round(in_sample_acc, 2))
+                # Version 4: Use a graph for monitoring
+                f.write(
+                    f"{MODEL_NAME},{round(time.time(), 3)},in_sample,{round(float(acc), 2)},{round(float(loss), 4)}\n")
+                """
+                Version 3:
+                Instead of dispalying like version 2, it saves it into a log file.
+                f.write(f"{MODEL_NAME}, {int(time.time())}, in_sample, {round(float(loss), 4)}\n")
+                """
+
+                """
+                Version 2:
+                prints this all out every step.
+                print(f"In - sample acc: {round(float(acc), 2)} Loss: {round(float(loss), 2)}")
+                """
+
+                """
+                Version 1:
+                Has too many things repeated so it is better to define a fwd_pass function that will be called instead
+                of repeating all the bellow.
+                
+                net.zero_grad()
+                V1 outputs = net(batch_X)
+
+                matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, batch_y)]
+                in_sample_acc = matches.count(True) / len(matches)
+
+                loss = loss_function(outputs, batch_y)
+                loss.backward()
+                optimizer.step()
+            print(loss)
+            print("In-sample acc:", round(in_sample_acc, 2))
+            """
 
 
 train(net)
+
+
+style.use("ggplot")
+
+model_name = "model-1579796306"
+
+
+def create_acc_loss_graph(model_name):
+    contents = open("model.log", "r").read().split("\n")
+
+    times = []
+    accuracies = []
+    losses = []
+
+    for c in contents:
+        if model_name in c:
+            name, timestamp, sample_type, acc, loss = c.split(",")
+
+            times.append(timestamp)
+            accuracies.append(acc)
+            losses.append(loss)
+
+    fig = plt.figure()
+
+    ax1 = plt.subplot2grid((2, 1), (0, 0))
+    ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
+
+    ax1.plot(times, accuracies, label='in_samp_acc')
+    ax1.legend(loc=2)
+    ax2.plot(times, losses, label='in_sample_loss')
+    ax2.legend(loc=2)
+    plt.show()
+
+
+create_acc_loss_graph(model_name)
 
 
 def test(net):
